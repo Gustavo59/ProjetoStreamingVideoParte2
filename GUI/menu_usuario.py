4229b2b37b3998b4fd56649fd92ab2bc3fc3921d
Gustavo Barros <gustavosbarros59@gmail>
2017-10-13 19:24:40 -0300

    Codificando a função imprimir_usuario do modulo menu_usuario

---
+++ GUI/menu_usuario.py
@@ -0,0 +1,8 @@
+from LOGICA import usuario
+
+def imprimir_usuario(usuario):
+    print ("CPF: ",  usuario[0])
+    print ("Nome: ", usuario[1])
+    print ("Email: ",  usuario[2])
+    print("Senha: ", usuario[3])
+    print ()
