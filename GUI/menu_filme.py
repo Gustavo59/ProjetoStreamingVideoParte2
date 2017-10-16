d3192e6582748c28e2e84c172ecdd58b865938cf
Andre Vinicius <info.andre.vinicius@gmail.com>
2017-10-16 11:20:48 -0200

    menus listar, buscar e remover adicionados

--- GUI/menu_filme.py
+++ GUI/menu_filme.py
@@ -18,5 +18,24 @@ def menu_listar():
     for f in filmes:
         imprimir_filme(f)
 
+   
+def menu_buscar():
+    print ("\nBuscar Filme por Código \n")
+    cod = int(input("Código: "))
+    print()
+    f = filme.buscar_filme(cod)
+    if (f == None):
+        print ("Filme não encontrado no catálogo ")
+    else:
+        imprimir_filme(f)
 
-    
+
+def menu_remover():
+    print ("\nRemover Filme \n")
+    cod = int(input("Código: "))
+    print()
+    f = filme.remover_filme(cod)
+    if (f == False):
+        print ("Filme não encontrado")
+    else:
+        print ("Filme removido")
